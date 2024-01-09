6d221584a03524021b8e7418114e633a1def7b08
klaw <netke2611@gmail.com>
2024-01-09 23:56:33 +0530

    blog fix

--- pages/api/blog/[id].js
+++ pages/api/blog/[id].js
@@ -19,8 +19,9 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
       const blogID = req.query.id;
-      const blogData = await BlogD.findOne({ _id: blogID });
-
+      const blogData = await BlogD.findOne({ _id: blogID }).populate(
+        "reviews"
+      );
       if (blogData) {
         return res.status(200).json({
           data: blogData,
